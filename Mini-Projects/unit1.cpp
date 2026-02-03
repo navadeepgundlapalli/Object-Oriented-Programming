68f60d0cad5c039c541244f33dcab2440f6f202f
navadeepgundlapalli <gundlapalli.137523@marwadiuniversity.ac.in>
2026-02-03 12:59:29 +0530

    Add billing program for vegetables

---
+++ Mini-Projects/unit1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int count = 0, vegs;
+    float rate, weight;
+    float grandTotal = 0, itemTotal;
+
+    cout << "How many vegetables do you want to bill? ";
+    cin >> vegs;
+
+    while(count < vegs) {
+        cout << "\nEnter price per kg: ";
+        cin >> rate;
+
+        cout << "Enter weight (kg): ";
+        cin >> weight;
+
+        itemTotal = rate * weight;
+        grandTotal = grandTotal + itemTotal;
+
+        count++;
+    }
+
+    cout << "\nFinal Amount to Pay = " << grandTotal << endl;
+
+    return 0;
+}
