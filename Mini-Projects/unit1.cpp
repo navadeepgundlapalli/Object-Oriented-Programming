0901735081647081c98caecf0363d5d4d496f1ef
navadeepgundlapalli <gundlapalli.137523@marwadiuniversity.ac.in>
2026-02-12 12:10:05 +0530

    Smart billing Machine
    Added GST calculation to the vegetable billing system.

--- Mini-Projects/unit1.cpp
+++ Mini-Projects/unit1.cpp
@@ -5,24 +5,47 @@ int main() {
     int count = 0, vegs;
     float rate, weight;
     float grandTotal = 0, itemTotal;
+    float gstAmount, finalTotal;
 
-    cout << "How many vegetables do you want to bill? ";
+     const float GST_RATE = 0.05;
+
+    cout << "====================================\n";
+    cout << "        VEGETABLE BILLING SYSTEM     \n";
+    cout << "====================================\n";
+
+    cout << "Enter number of vegetables to bill: ";
     cin >> vegs;
 
-    while(count < vegs) {
-        cout << "Enter price per kg: ";
-        cin >> rate;
+    cout << "------------------------------------\n";
 
+    while(count < vegs) {
+        cout << "Item " << count + 1 << endl;
+        cout << "------------------------------------\n";
+        cout << "Enter price per kg (Rs): ";
+        cin >> rate;
         cout << "Enter weight (kg): ";
         cin >> weight;
 
         itemTotal = rate * weight;
-        grandTotal = grandTotal + itemTotal;
+        grandTotal += itemTotal;
+
+        cout << "Item Total = Rs " << itemTotal << endl;
+        cout << "------------------------------------\n";
 
         count++;
     }
+    gstAmount = grandTotal * GST_RATE;
+    finalTotal = grandTotal + gstAmount;
 
-    cout << "Final Amount to Pay = " << grandTotal << endl;
+    cout << "====================================\n";
+    cout << "        FINAL BILL SUMMARY           \n";
+    cout << "====================================\n";
+
+    cout << "Subtotal = Rs " << grandTotal << endl;
+    cout << "GST (5%) = Rs " << gstAmount << endl;
+    cout << "Total Amount to Pay = Rs " << finalTotal << endl;
+
+    cout << "====================================\n";
 
     return 0;
 }
